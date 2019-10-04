b59a50f6fd14869da632662cdbb8aaf5c2ebbab5
Kenpachi8 <55574595+Kenpachi8@users.noreply.github.com>
2019-10-04 20:06:30 +0700

    Add files via upload

--- Калькулятор/script.js
+++ Калькулятор/script.js
@@ -1,7 +1,7 @@
 let button = document.querySelector('input');
 button.addEventListener('keyup', Calculate)
 
-function inputEditor(input){
+function inputEditor(input) {
 
 	let text = '';
 
@@ -17,14 +17,54 @@ function inputEditor(input){
 
 	return text;
 }
+function resultEditor(str) {
+	if (str == undefined) {
+		str = '= 0';
+		return str;
+	}
+
+	else if (str == Infinity) {
+		str = '\u221E'
+		return str; 
+	}
+
+	else if (str == -Infinity) {
+		str = '-\u221E'
+		return str; 
+	}
+
+	return str;
+}
+/*
+function inputEditor(input){
+
+	let text = '';
+	return text;
+}
+*/
 
 function Calculate () {
 	let input = document.querySelector('input').value;
 	let answer = document.querySelector('.answer');
 	input = inputEditor(input);
-	result = eval(input);
-	answer.innerHTML = '';
-	if (result != undefined){
-		answer.insertAdjacentText('afterbegin', result);
+
+	try {
+		result = eval(input);
 	}
+
+	catch {
+		result = "Ошибка ввода"
+		answer.innerHTML = '';
+		answer.insertAdjacentText('afterbegin', result);
+		return 0;
+	}
+
+	console.log(result);
+
+	result = resultEditor(result);
+	console.log('  ' + result);
+
+	answer.innerHTML = '';
+	answer.insertAdjacentText('afterbegin', result);
+	
 }
