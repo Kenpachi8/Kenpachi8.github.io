bfa99e8e8a80d718be214b6a666e95386d09cbc5
Kenpachi8 <55574595+Kenpachi8@users.noreply.github.com>
2019-10-05 12:00:37 +0700

    Add files via upload

--- Калькулятор/script.js
+++ Калькулятор/script.js
@@ -1,6 +1,6 @@
 let button = document.querySelector('input');
-button.addEventListener('keyup', Calculate)
-
+button.addEventListener('keyup' || 'keydown', Calculate)
+/*
 function inputEditor(input) {
 
 	let text = '';
@@ -17,6 +17,7 @@ function inputEditor(input) {
 
 	return text;
 }
+*/
 function resultEditor(str) {
 	if (str == undefined) {
 		str = '= 0';
@@ -35,13 +36,44 @@ function resultEditor(str) {
 
 	return str;
 }
-/*
+
 function inputEditor(input){
 
 	let text = '';
+	input = input.split('');
+
+	if (!(('0' <= input[input.length-1]) && (input[input.length-1] <= '9'))) {
+		input.splice(input.length-1, 1);
+	}
+
+	for (let i = 0; i < input.length; i++) {
+
+		if (!(('0' <= input[i]) 
+			&& (input[i] <= '9')) 
+			&& (input[i] == input[i + 1])) {
+			if (input[i] == '-') {
+				input[i] = '+';
+				input.splice(i + 1, 1);
+			}
+			
+			else {
+				input.splice(i, 1);
+				i--;
+			}
+
+			} 
+
+		else if (input[i] == ':') {
+			input[i] = '/';
+		}
+	}
+
+	for (let i = 0; i < input.length; i++) {
+		text += input[i];
+	}
 	return text;
 }
-*/
+
 
 function Calculate () {
 	let input = document.querySelector('input').value;
