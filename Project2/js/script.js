0a13cf679dada29653a8162e3f7e34acd1f2c795
Kenpachi8 <55574595+Kenpachi8@users.noreply.github.com>
2019-11-23 14:46:09 +0700

    Add files via upload

--- Project2/js/script.js
+++ Project2/js/script.js
@@ -1,58 +1,37 @@
-let pages = document.querySelectorAll(".pages");
+/*размер текста под размер блока*/
+let pages = document.querySelectorAll(".page");
+let menu = document.querySelectorAll(".menu");
 let pagesList = ['.neple','.main','.price','.about'];
-for (let i = 0; i < pages.length; i++) {
-	pages[i].addEventListener('click', function () {
-		for (let j = 0; j < pages.length; j++) {
-			if (i == j) {
-				document.querySelector(pagesList[j]).classList.remove('hide');
-			}
-			else{
-				document.querySelector(pagesList[j]).classList.add('hide');
-			}
-		}
-	})
-}	
+console.log(pages);
 
-let img = document.querySelectorAll(".main img");
-
-function fullscreenImg(e) {
-	let imgSection = document.createElement('div');
-	let img = document.createElement('img');
-	let exit = document.createElement('div');
-	imgSection.classList.add('fullScreenPhotoDiv');
-	img.src = e.target.src;
-	img.classList.add('fullScreenPhoto');
-	exit.classList.add('exit');
-	exit.textContent = '\u00D7';
-
-	imgSection.addEventListener('click', function (e) {
-		if(e.target.className != 'fullScreenPhoto'){
-			console.log(e.target.className);
-			imgSection.style.display = 'none';
-		}
-	})
-
-	document.querySelector('.main').prepend(imgSection);
-	imgSection.append(img);
-	imgSection.append(exit);
-	console.log('work');
-	console.log(e.target);
-	console.log(exit);
+function Hider(pageName) {
+	let allHeirs = pageName.getElementsByTagName('*');
+	for (let i = allHeirs.length - 1; i >= 0; i--) {
+		//console.log(allHeirs);
+		allHeirs[i].classList.add('hide');
+	}
+	pageName.classList.add('hide');
 }
 
-	for (let i = img.length - 1; i >= 0; i--) {
-		img[i].addEventListener('click', fullscreenImg);
+function Shower(pageName) {
+	let allHeirs = pageName.getElementsByTagName('*');
+	for (let i = allHeirs.length - 1; i >= 0; i--) {
+		//console.log(allHeirs);
+		allHeirs[i].classList.remove('hide');
 	}
+	pageName.classList.remove('hide');
+}
 
-let galery = document.querySelector('.galery');
-let scrollBar = document.querySelector('.horizontalScrollBar');
-scrollBar.oninput = scroll;
 
-function scroll () {
-	let galeryWidth = galery.clientWidth;
-	let screenWidth = document.documentElement.clientWidth;
-	console.log(screenWidth);
-	let step = (galeryWidth-screenWidth)/100;
-	document.querySelector('.galery').style.marginLeft = -step*scrollBar.value +'px';
-	console.log(galeryWidth);
+for (let i = 0; i < menu.length; i++) {
+	menu[i].addEventListener('click', function () {
+		for (let j = 0; j < menu.length; j++) {
+			if (i == j) {
+				Shower(pages[j]);
+			}
+			else{
+				Hider(pages[j]);
+			}
+		}
+	})
 }
