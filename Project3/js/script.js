42aff2cf57f75778bd3c7f64768f6737339506fe
Kenpachi8 <55574595+Kenpachi8@users.noreply.github.com>
2019-11-25 21:46:23 +0700

    Add files via upload

--- Project3/js/script.js
+++ Project3/js/script.js
@@ -2,20 +2,28 @@ let screenHeight = document.documentElement.clientHeight;
 document.querySelector('.box3').style.width = 
 	document.querySelector('.box2').clientWidth + 
 	document.querySelector('.box1').clientWidth + 20 +'px'
-
 document.querySelector('.questions').style.height = 
 	document.querySelector('.question_container').clientHeight + 'px'
+
+window.addEventListener('orientationchange', function () {
+	document.querySelector('.box3').style.width = 
+		document.querySelector('.box2').clientWidth + 
+		document.querySelector('.box1').clientWidth + 20 +'px'
+	document.querySelector('.questions').style.height = 
+		document.querySelector('.question_container').clientHeight + 'px'	 
+})
 
 function getYCoordinats(elem) {
   return elem.getBoundingClientRect().top + pageYOffset;
 }
 
 window.addEventListener('scroll', function() {
-	console.log(window.scrollY);
+	//console.log(window.scrollY);
 	if(window.scrollY > 10){
 		document.querySelector('.main_text').style.marginTop = 0 + 'px';
 		document.querySelector('.gradient').style.opacity = '1';
 		setTimeout(function () {
+			document.querySelector('hr').style.display = 'block';
 			document.querySelector('hr').style.width = document.querySelector('.neple_layer').clientWidth + 'px';
 		}, 1500)
 	}
@@ -28,11 +36,20 @@ window.addEventListener('scroll', function() {
 				window.addEventListener('scroll', function() {
 				setTimeout(function () {
 					document.querySelectorAll('.gradient2').forEach(item => item.style.marginTop = 0 + 'px');
-				}, 1400)
+				}, 1000)
 			});
-		}, 400)
+		}, 1000)
 	}
 
 });
 
+document.querySelectorAll('.gradient2 a').forEach(item => item.addEventListener('click', modalWindow));
+
+function modalWindow(){
+	document.querySelector('.modal').style.display = 'block';
+}
+
+document.querySelector('.exit').addEventListener('click', function() {
+	document.querySelector('.modal').style.display = 'none';
+})
 
