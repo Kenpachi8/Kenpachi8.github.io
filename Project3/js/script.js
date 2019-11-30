2f2830069d737a75d222a5b6f10c795a47f8a7f1
Kenpachi8 <55574595+Kenpachi8@users.noreply.github.com>
2019-11-30 23:03:30 +0700

    Add files via upload

--- Project3/js/script.js
+++ Project3/js/script.js
@@ -5,6 +5,14 @@ document.querySelector('.box3').style.width =
 document.querySelector('.questions').style.height = 
 	document.querySelector('.question_container').clientHeight + 'px'
 
+document.querySelector('.question_container').style.marginTop = document.querySelector('.questions').clientHeight + 'px'
+
+document.querySelector('.main_text').style.marginTop = 
+	document.querySelector('.main').clientHeight + 
+	document.querySelector('.nav').clientHeight+
+	'px'
+
+
 window.addEventListener('orientationchange', function () {
 	document.querySelector('.box3').style.width = 
 		document.querySelector('.box2').clientWidth + 
@@ -12,10 +20,6 @@ window.addEventListener('orientationchange', function () {
 	document.querySelector('.questions').style.height = 
 		document.querySelector('.question_container').clientHeight + 'px'	 
 })
-
-function getYCoordinats(elem) {
-  return elem.getBoundingClientRect().top + pageYOffset;
-}
 
 window.addEventListener('scroll', function() {
 	//console.log(window.scrollY);
@@ -25,7 +29,7 @@ window.addEventListener('scroll', function() {
 		setTimeout(function () {
 			document.querySelector('hr').style.display = 'block';
 			document.querySelector('hr').style.width = document.querySelector('.neple_layer').clientWidth + 'px';
-		}, 1500)
+		}, 1200)
 	}
 	let questions = document.querySelector('.questions');
 	let questionsY = getYCoordinats(questions)
@@ -33,15 +37,14 @@ window.addEventListener('scroll', function() {
 	if (window.scrollY > questionsY - screenHeight){
 		setTimeout(function () {
 			document.querySelector('.question_container').style.marginTop = 0 + 'px';
-				window.addEventListener('scroll', function() {
-				setTimeout(function () {
-					document.querySelectorAll('.gradient2').forEach(item => item.style.marginTop = 0 + 'px');
-				}, 1000)
-			});
+			document.querySelector('.gradient3').style.opacity = '0.8';
 		}, 1000)
 	}
-
 });
+
+function getYCoordinats(elem) {
+  return elem.getBoundingClientRect().top + pageYOffset;
+}
 
 document.querySelectorAll('.gradient2 a').forEach(item => item.addEventListener('click', modalWindow));
 
